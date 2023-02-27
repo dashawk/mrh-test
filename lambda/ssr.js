6b953c1dc55aa99ea59a11ca601e932f55445119
Jason P <jmpanugaling@gmail.com>
2023-02-27 20:37:37 +0800

    ssr fix

--- lambda/ssr.js
+++ lambda/ssr.js
@@ -2,23 +2,24 @@ import fs from 'node:fs/promises'
 
 export const handler = async (req, res) => {
 	const { url } = req
-	const template = await fs.readFile(`../dist/client/index.html`, 'utf-8')
-	const manifest = await fs.readFile(
-		`../dist/client/ssr-manifest.json`,
-		'utf-8'
-	)
 
-	const render = (await import(`../dist/server/entry-server.js`)).render
+	// const template = await fs.readFile(`../dist/client/index.html`, 'utf-8')
+	// const manifest = await fs.readFile(
+	// 	`../dist/client/ssr-manifest.json`,
+	// 	'utf-8'
+	// )
 
-	const appHtml = await render(url, manifest)
+	// const render = (await import(`../dist/server/entry-server.js`)).render
+
+	// const appHtml = await render(url, manifest)
 
 	console.log(`URL`, url)
-	console.log(`Template`, template)
-	const html = template
-		.replace(`<!--app-head-->`, appHtml.head ?? '')
-		.replace(`<!--app-html-->`, appHtml.html ?? '')
+	// console.log(`Template`, template)
+	// const html = template
+	// 	.replace(`<!--app-head-->`, appHtml.head ?? '')
+	// 	.replace(`<!--app-html-->`, appHtml.html ?? '')
 
 	res.statusCode = 200
 	res.setHeader(`Content-Type`, 'text/html')
-	res.end(html)
+	res.end(`<p>Hello World</p>`)
 }
